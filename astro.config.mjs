978dd9d31f2178f0d7c425fe10081d6774827982
James <jamesfleming.sd@gmail.com>
2025-04-27 20:23:26 -0700

    just redid the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from "astro/config";
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://james-fleming12.github.io',
+    integrations: [mdx(), sitemap()],
+    markdown: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex]
+    }
+});
---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Red-Team Bench';
+export const SITE_DESCRIPTION = 'Project site for JM Research Red-Team based Model Evaluation';
